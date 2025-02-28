e295e28ae4b9481e2278a0fccf1dd4eba8855349
应卓 <yingzhor@gmail.com>
2025-02-28 20:17:25 +0800

    2025-02-28 20:17:25

--- projects-application/core/src/main/java/com/mycompany/myproject/core/ApplicationBoot.kt
+++ projects-application/core/src/main/java/com/mycompany/myproject/core/ApplicationBoot.kt
@@ -2,7 +2,9 @@ package com.mycompany.myproject.core
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ImportResource
 
+@ImportResource("classpath:config/groovy-dsl.groovy")
 @SpringBootApplication
 open class ApplicationBoot
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,12 +15,12 @@ pluginManagement {
 dependencyResolutionManagement {
 	repositories {
 		mavenLocal()
-//		maven { name = "腾讯云"; url = uri("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/") }
-//		maven { name = "阿里云"; url = uri("https://maven.aliyun.com/repository/public/") }
-//		maven { name = "华为云"; url = uri("https://repo.huaweicloud.com/repository/maven/") }
-//		maven { name = "Spring(GA)"; url = uri("https://repo.spring.io/release") }
-//		maven { name = "Spring(Milestone)"; url = uri("https://repo.spring.io/milestone") }
-//		maven { name = "Spring(Snapshot)"; url = uri("https://repo.spring.io/snapshot") }
+		maven { name = "腾讯云"; url = uri("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/") }
+		maven { name = "阿里云"; url = uri("https://maven.aliyun.com/repository/public/") }
+		maven { name = "华为云"; url = uri("https://repo.huaweicloud.com/repository/maven/") }
+		maven { name = "Spring(GA)"; url = uri("https://repo.spring.io/release") }
+		maven { name = "Spring(Milestone)"; url = uri("https://repo.spring.io/milestone") }
+		maven { name = "Spring(Snapshot)"; url = uri("https://repo.spring.io/snapshot") }
 		mavenCentral()
 		google()
 	}
