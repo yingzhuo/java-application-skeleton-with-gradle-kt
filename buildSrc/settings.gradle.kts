b94ad01e1f89447b913674cd93757ebee5dbb0f3
应卓 <yingzhor@gmail.com>
2025-03-04 14:37:29 +0800

    2025-03-04 14:37:28

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -24,6 +24,7 @@ pluginManagement {
 		maven { name = "腾讯云"; url = uri("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/") }
 		maven { name = "阿里云"; url = uri("https://maven.aliyun.com/repository/public/") }
 		maven { name = "华为云"; url = uri("https://repo.huaweicloud.com/repository/maven/") }
+		maven { name = "网易云"; url = uri("https://mirrors.163.com/maven/repository/maven-public/") }
 		mavenCentral()
 		google()
 	}
@@ -41,6 +42,7 @@ dependencyResolutionManagement {
 		maven { name = "腾讯云"; url = uri("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/") }
 		maven { name = "阿里云"; url = uri("https://maven.aliyun.com/repository/public/") }
 		maven { name = "华为云"; url = uri("https://repo.huaweicloud.com/repository/maven/") }
+		maven { name = "网易云"; url = uri("https://mirrors.163.com/maven/repository/maven-public/") }
 		maven { name = "Spring(GA)"; url = uri("https://repo.spring.io/release") }
 		maven { name = "Spring(Milestone)"; url = uri("https://repo.spring.io/milestone") }
 		maven { name = "Spring(Snapshot)"; url = uri("https://repo.spring.io/snapshot") }
